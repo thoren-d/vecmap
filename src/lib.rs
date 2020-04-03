6ddc7f90162a4129182a8de4ecc5672a28a65b56
Thoren Paulson <thoren.paulson@gmail.com>
2020-04-03 01:07:15 -0700

    Revert "Add a failing test."
    This reverts commit 0dc7d4e037e62f2f5da04e31bf53c0fb4dbc4b92.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,7 @@
-
-pub fn foo() -> i32 {
-    42
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn it_doesnt_work() {
-        assert_eq!(5, foo());
     }
 }
