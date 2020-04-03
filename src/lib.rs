0dc7d4e037e62f2f5da04e31bf53c0fb4dbc4b92
Thoren Paulson <thoren.paulson@gmail.com>
2020-04-03 01:01:07 -0700

    Add a failing test.
    Just to play with Travis setup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+
+pub fn foo() -> i32 {
+    42
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_doesnt_work() {
+        assert_eq!(5, foo());
     }
 }
